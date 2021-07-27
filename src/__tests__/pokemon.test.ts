5bd7edf863a9bd5f68e58fe4426a59a7028cfef3
JuniorN1 <junior_e.a.o@hotmail.com>
2021-07-27 00:15:58 -0300

    first test with swagger and git actions

--- src/__tests__/pokemon.test.ts
+++ src/__tests__/pokemon.test.ts
@@ -1,10 +1,10 @@
 import request from 'supertest'
 import  app  from '../app'
 describe("Pokemos", ()=>{
-    it("Should be able to create a new surveys",async ()=>{
+    it("if get pokemon?",async ()=>{
         const response = await request(app)
         .get("/v1/pokemons");        
         expect(response.status).toBe(200);
-        // expect(response.body).toHaveProperty("name",);
+         expect(response.body).toHaveProperty("name");
 })
 })
