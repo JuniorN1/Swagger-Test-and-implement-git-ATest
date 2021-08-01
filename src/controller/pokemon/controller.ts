9ac555838bb63d1e8d0bb7a45762412866a06ba4
JuniorN1 <junior_e.a.o@hotmail.com>
2021-08-01 15:24:49 -0300

    create migrate , seeders, controller

---
+++ src/controller/pokemon/controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import connection from '../../connection';
+class pokemon{
+
+    async show(request:Request,response:Response){
+       
+
+    }
+
+}
+
